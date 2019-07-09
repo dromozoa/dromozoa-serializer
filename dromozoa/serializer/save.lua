e97a134fa9422fde17b6898ed15915e8da090460
Tomoyuki Fujimori <moyu@dromozoa.com>
2019-07-09 09:38:54 +0900

    リファクタリング。

--- dromozoa/serializer/save.lua
+++ dromozoa/serializer/save.lua
@@ -70,75 +70,7 @@ local function write(handle, u, map, max)
   return max
 end
 
-local function build(buffer, i, u, map, max)
-  local t = type(u)
-  if t == "boolean" then
-    if u then
-      i = i + 1
-      buffer[i] = "1"
-    else
-      i = i + 1
-      buffer[i] = "2"
-    end
-  elseif t == "number" then
-    if math_type and math_type(u) == "integer" then
-      i = i + 1
-      buffer[i] = "3"
-      i = i + 1
-      buffer[i] = u
-    elseif u % 1 == 0 then
-      i = i + 1
-      buffer[i] = "4"
-      i = i + 1
-      buffer[i] = u
-    else
-      i = i + 1
-      buffer[i] = "4"
-      i = i + 1
-      buffer[i] = ("%.17g"):format(u)
-    end
-  elseif t == "string" then
-    i = i + 1
-    buffer[i] = "5"
-    i = i + 1
-    buffer[i] = #u .. ":" .. u
-  elseif t == "table" then
-    local ref = map[u]
-    if ref then
-      i = i + 1
-      buffer[i] = "6"
-      i = i + 1
-      buffer[i] = ref
-    else
-      max = max + 1
-      i = i + 1
-      buffer[i] = "7"
-      i = i + 1
-      buffer[i] = max
-      map[u] = max
-      for k, v in pairs(u) do
-        i, max = build(buffer, i, k, map, max)
-        i, max = build(buffer, i, v, map, max)
-      end
-      i = i + 1
-      buffer[i] = "8"
-    end
-  end
-  return i, max
-end
-
 return function (handle, u)
   handle:write "1\n"
-  -- print(collectgarbage "count")
   write(handle, u, {}, 0)
-  -- local buffer = {}
-  -- build(buffer, 0, u, {}, 0)
-  -- print(collectgarbage "count")
-  -- for i = 1, #buffer do
-  --   handle:write(buffer[i], "\n")
-  -- end
-  -- local s = table.concat(buffer, "\n")
-  -- print(collectgarbage "count")
-  -- handle:write(s, "\n")
-  -- print(collectgarbage "count")
 end
